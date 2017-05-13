c657a8296da89d2d7655016f311cee63f8afb46a
William Burns <william@williamburns.co.uk>
2017-05-13 17:41:17 +0100

    Create design for login page.

--- client-desktop/index.ts
+++ client-desktop/index.ts
@@ -1,4 +1,6 @@
 import electron = require("electron");
+import url = require("url");
+import path = require("path");
 const { app, BrowserWindow } = electron;
 
 declare var __dirname, process;
@@ -7,10 +9,15 @@ let mainWindow;
 
 function createWindow() {
 	mainWindow = new BrowserWindow({
-		width: 1200, height: 600
+		width: 1200, height: 600,
+		titleBarStyle: 'hidden'
 	});
 
-	mainWindow.loadURL('http://google.com');
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'public/html/auth/login.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
 
 	mainWindow.on('closed', () => {
 		mainWindow = null;
